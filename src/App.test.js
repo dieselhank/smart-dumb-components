2ab2009a027b2d8d8daa6826de0e15867a8e4cda
David Specht <David.Specht@shiftwise.com>
2020-01-22 15:18:27 -0800

    Fix App unit test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders instructions', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Select one of the links above/i);
   expect(linkElement).toBeInTheDocument();
 });
